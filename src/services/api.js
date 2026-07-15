66482d9021a169ffebf73e14af5b2d6fcbbd6460
Ankit Sinha <YOUR_GITHUB_EMAIL>
2026-07-15 10:54:00 +0530

    api url change

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8001/documents",
+  /*  baseURL: "http://127.0.0.1:8001/documents", */
+  baseURL: "https://document-ai-backend-923w.onrender.com/documents/",
   headers: {
     "Content-Type": "application/json",
   },
